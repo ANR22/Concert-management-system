03e8c444a3c5c03b100ceba645e2dc125c6c788b
Amogh N Rao <amogh090502@gmail.com>
2022-11-12 19:44:41 +0530

    populated database with insert statements

--- sql_statements/DDL.sql
+++ sql_statements/DDL.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Concert
 (
+  concert_id INT NOT NULL,
   concert_name VARCHAR(50) NOT NULL,
   Date_time Timestamp NOT NULL,
-  concert_id INT NOT NULL,
   theme VARCHAR(50) NOT NULL,
   city VARCHAR(50) NOT NULL,
   venue VARCHAR(50) NOT NULL,
@@ -11,19 +11,19 @@ CREATE TABLE Concert
 
 CREATE TABLE Artist
 (
+  artist_id INT NOT NULL,
   artist_name VARCHAR(20) NOT NULL,
   num_of_songs INT NOT NULL,
-  artist_id INT NOT NULL,
   gender enum('Male','Female','Other') NOT NULL,
   PRIMARY KEY (artist_id)
 );
 
 CREATE TABLE User
 (
-  Name VARCHAR(20) NOT NULL,
   user_id INT NOT NULL,
+  Name VARCHAR(20) NOT NULL,
   DOB DATE NOT NULL,
-  ph_no INT NOT NULL,
+  ph_no VARCHAR(10) NOT NULL,
   is_admin enum('yes','no') NOT NULL,
   PRIMARY KEY (user_id)
 );
@@ -31,30 +31,30 @@ CREATE TABLE User
 CREATE TABLE Ticket
 (
   Ticket_id INT NOT NULL,
-  Price Float NOT NULL,
   Seat_no INT NOT NULL,
+  Price Float NOT NULL,
   concert_name VARCHAR(20) NOT NULL,
   ticket_type enum('gold','platinum','vip') NOT NULL,
   concert_id INT NOT NULL,
-  PRIMARY KEY (Ticket_id,Seat_no),
+  PRIMARY KEY (Ticket_id),
   FOREIGN KEY (concert_id) REFERENCES Concert(concert_id)
 );
 
 CREATE TABLE properties
 (
+  concert_id INT NOT NULL,
   prop_name VARCHAR(50) NOT NULL,
   quantity INT NOT NULL,
   price_per_unit FLOAT NOT NULL,
-  concert_id INT NOT NULL,
-  PRIMARY KEY (concert_id),
+  PRIMARY KEY (concert_id,prop_name),
   FOREIGN KEY (concert_id) REFERENCES Concert(concert_id)
 );
 
 CREATE TABLE performed_by
 (
-  fee FLOAT NOT NULL,
   concert_id INT NOT NULL,
   artist_id INT NOT NULL,
+  fee FLOAT NOT NULL,
   PRIMARY KEY (concert_id, artist_id),
   FOREIGN KEY (concert_id) REFERENCES Concert(concert_id),
   FOREIGN KEY (artist_id) REFERENCES Artist(artist_id)
---
+++ sql_statements/insert/properties-insert.sql
@@ -0,0 +1,19 @@
+'mike'
+'speaker'
+'screen'
+
+
+insert into properties value (10001,'mike',10,1500); 
+insert into properties value (10002,'mike',7,1500); 
+insert into properties value (10003,'mike',9,1500); 
+insert into properties value (10004,'mike',5,1500); 
+insert into properties value (10005,'mike',4,1500); 
+insert into properties value (10001,'speaker',12,10000); 
+insert into properties value (10002,'speaker',15,10000); 
+insert into properties value (10003,'speaker',11,10000); 
+insert into properties value (10001,'screen',10,10000); 
+insert into properties value (10002,'screen',5,10000); 
+insert into properties value (10003,'screen',3,10000); 
+insert into properties value (10004,'screen',4,10000); 
+insert into properties value (10005,'screen',2,10000); 
+
